2d99b3d7c582b75d5376ab926f5cac9f4b3949d3
Kierra Westmoreland <kw324307@ohio.edu>
2017-01-07 00:55:16 -0500

    passed test & changed tests & methods to startTimeLimit

--- src/babySitterKata/NightlyRate.java
+++ src/babySitterKata/NightlyRate.java
@@ -5,13 +5,16 @@ public class NightlyRate {
 	private int rateStartToBedtime;
 	private int rateBedtimeToMidnight;
 	private int rateMidnightToEnd;
-	private int startTimeLimit; //military time
+	private int startTimeLimit = 17; //military time
 	private int endTimeLimit;
 	private int startTime;
 	private int endTime;
 	
-	public int getStartTime(int startTime) {
+	
+	public int getStartTimeLimit() {
+		return startTimeLimit;
+	}
+	
 
-		return 0;}
 	
 }
--- test/babySitterKata/NightlyRateTest.java
+++ test/babySitterKata/NightlyRateTest.java
@@ -11,8 +11,8 @@ public class NightlyRateTest {
 	@Test
 	public void getBabySitterStartTime(){
 		NightlyRate underTest = new NightlyRate();
-		int startTime = underTest.getStartTime(1);
-		assertEquals(5, startTime);
+		int startTime = underTest.getStartTimeLimit();
+		assertEquals(17, startTime);
 	}
 	
 }
